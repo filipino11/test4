f83791ca355df36274eb9822e4394f5f0298f462
Tománek Filip <Tomanek.Fi.2022@skola.ssps.cz>
2022-11-09 10:51:46 +0100

    first commit,

---
+++ ConsoleApp_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int num01 = int.Parse(Console.ReadLine());
+
+                for (int j = 0; i < num01; i++)
+                {
+                    Console.WriteLine("ahoj");
+                }
+            }
+        }
+    }
+}
